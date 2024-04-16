2a4c06c90086a64b053c9c7241c099ebee4ba830
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-04-17 00:26:10 +0530

    Math functions

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 # print('Sanket Pujari');
 # print(type(6));
 # input('What is the name ?');
-name = input('What is the name?');
-print('Heeeellloooo ' + name);
-# print(round(3.1));
+# name = input('What is the name?');
+# print('Heeeellloooo ' + name);
 
 # the below code fragment can be found in:
+
+# Fundamental Data Types
+# int and float
+
+print(type(6));
+print(type(2-4));
+print(type(2*4));
+print(type(2/4));
+
+print(2 ** 3); # 8
+print(5 // 4); # 1
+print(6%4); # 2
+
+#  
+
+print(round(3.1));# 3
+print(abs(-20)); #20
+
+print(pow(2, 3)); # 8
+
+print(max(2, 4)); # 4
+
+print(min(2, 4)); # 2
+
+print(int(3.1)); # 3
+
+print(float(3)); # 3.0
+
+print(round(3.1, 1)); # 3.1
+
+print(round(3.1, 2)); # 3.10

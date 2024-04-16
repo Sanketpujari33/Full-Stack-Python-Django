00e0aca4b4fbfe72620851429050cb6314bd5b86
Sanket Arjun Pujari <104687744+Sanketpujari33@users.noreply.github.com>
2024-04-17 00:07:46 +0530

    Frist make a code

---
+++ main.py
@@ -0,0 +1,8 @@
+# print('Sanket Pujari');
+# print(type(6));
+# input('What is the name ?');
+name = input('What is the name?');
+print('Heeeellloooo ' + name);
+# print(round(3.1));
+
+# the below code fragment can be found in:
